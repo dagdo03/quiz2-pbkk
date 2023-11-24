c31c35e139e7a7c9a2b234c2ce36cca4d99253f7
dagdo03 <ihsan.widagdo@gmail.com>
2023-11-24 21:55:08 +0700

    feat: add new bookstore project

---
+++ bookstore-2/app/Config/Validation.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Config;
+
+use CodeIgniter\Config\BaseConfig;
+use CodeIgniter\Validation\StrictRules\CreditCardRules;
+use CodeIgniter\Validation\StrictRules\FileRules;
+use CodeIgniter\Validation\StrictRules\FormatRules;
+use CodeIgniter\Validation\StrictRules\Rules;
+
+class Validation extends BaseConfig
+{
+    // --------------------------------------------------------------------
+    // Setup
+    // --------------------------------------------------------------------
+
+    /**
+     * Stores the classes that contain the
+     * rules that are available.
+     *
+     * @var string[]
+     */
+    public array $ruleSets = [
+        Rules::class,
+        FormatRules::class,
+        FileRules::class,
+        CreditCardRules::class,
+    ];
+
+    /**
+     * Specifies the views that are used to display the
+     * errors.
+     *
+     * @var array<string, string>
+     */
+    public array $templates = [
+        'list'   => 'CodeIgniter\Validation\Views\list',
+        'single' => 'CodeIgniter\Validation\Views\single',
+    ];
+
+    // --------------------------------------------------------------------
+    // Rules
+    // --------------------------------------------------------------------
+    public $register = [
+		'username' => [
+			'rules' => 'required|min_length[5]',
+		],
+		'password' => [
+			'rules' => 'required',
+		],
+		'repeatPassword'=>[
+			'rules' => 'required|matches[password]',
+		],
+	];
+
+	public $register_errors = [
+		'username' => [
+			'required' =>'{field} Harus Diisi',
+			'min_length' => '{field} Minimal 5 Karakter',
+		],
+		'password' => [
+			'required' => '{field} Harus Diisi',
+		],
+		'repeatPassword'=>[
+			'required' => '{field} Harus Diisi',
+			'matches' => '{field} Tidak Match Dengan Password'
+		],
+	];
+
+	public $login = [
+		'username' => [
+			'rules' => 'required|min_length[5]',
+		],
+		'password' => [
+			'rules' => 'required',
+		],
+	];
+
+	public $login_errors = [
+		'username' => [
+			'required' =>'{field} Harus Diisi',
+			'min_length' => '{field} Minimal 5 Karakter',
+		],
+		'password' => [
+			'required' => '{field} Harus Diisi',
+		],
+	];
+}
